0030a21d8d88bbf5875b812a885e940adcbd5081
darwinchou <darwinc93@gmail.comgit config --global user.email darwinc93@gmail.comgit config --global user.email darwinc93@gmail.com>
2018-01-19 09:39:49 -0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+1. Associar as imagens a cada carta na face de trás
+2. Embaralhar as cartas
+3. clicar em duas cartas
+4. Comparar as cartas, se forem iguais a face continua virado, se nao vira para trás
+*/
+
+window.onload = function() {
+
+  const mistura_carta = function() {
+    let carta = document.getElementsByClassName('card');
+    let img = [img1.jpg, img1.jpg, img2.jpg, img2.jpg, img3.jpg, mg3.jpg, img4.jpg, img4.jpg, img5.jpg, img5.jpg];
+
+    }
+  }
+}
